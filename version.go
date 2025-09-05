8f4d8a164c1bfd938ac05f4d0e3863e90a94d136
Your Name <you@example.com>
2025-09-05 12:12:07 -0300

    Bump version to v2.0.1
    - Updated version.go to increment build number
    - Improved Docker build process with proper version management
    - Added git commit hash for debugging and support

--- version.go
+++ version.go
@@ -8,7 +8,7 @@ import (
 
 // Version information - updated during releases
 var (
-	Version   = "v2.0.0"
+	Version   = "v2.0.1"
 	BuildDate = "unknown"
 	GitCommit = "unknown"
 	Author    = "Girino Vey"
